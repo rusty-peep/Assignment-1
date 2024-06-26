e3a120fd5fc3dcb4aa038196f9b664a913cc182e
mahavir-dash <mahavir@swnglobal.com>
2024-06-26 21:47:46 +0530

    Add the questions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+///Here is your assignment for day 1 based on variables and datatypes. Please read the description written above
+/// the functions carefully and it tells what you need to do in the function. Make sure to print at everystep to see the output.
+
+
+// Print "Hello World!"
 fn main() {
-    println!("Hello, world!");
+   todo!()
+}
+
+// Declare a mutable integer variable named count and initialize it to 10 and then print the value.
+// Change the value of the mutable variable count to 20 and print it again.
+fn _q1(){
+    todo!()
+}
+
+// Declare an immutable string variable named name and initialize it to "Alice".
+fn _q2(){
+    todo!()
+}
+
+
+// Declare a tuple containing an integer, a float, and a string, and assign it to a variable named "my_tuple" and print the value of my_tuple.
+fn _q3(){
+    todo!()
+}
+
+// Declare a constant named PI with a value of 3.14159.
+fn _q4(){
+    todo!()
+}
+
+// Declare a mutable integer variable named count and initialize it to 10 and print it.
+fn _q5(){
+    todo!()
 }
